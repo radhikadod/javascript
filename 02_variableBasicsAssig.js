f254d02edf1cbfa5d0425e780f3ce08056b088da
radhikadod <radhikadod@gmail.com>
2023-05-06 16:17:07 +0530

    commit and push
    commit and push

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,25 @@
+var age = 26;
+console.log("Age:", age, "Type:" , typeof age);
+var price = 100;
+console.log("Price:", price, "Type:", typeof price);
+var mobileNumber =10;
+console.log("Mobile Number:", mobileNumber, "Type:", typeof mobileNumber);
+console.log("============================================================");
+var firstName = "Radhika";
+console.log("Name:",firstName, "Type:", typeof firstName);
+var color = "Black";
+console.log("Favourite color:",color , "Type:", typeof color);
+var technology = "Javascript";
+console.log("Technology:", technology, "Type:" , typeof technology);
+console.log("============================================================");
+var isMarried = false;
+console.log("Is Married:", isMarried, "Type:", typeof isMarried);
+isSoftwareEngineer = true;
+console.log("Is Software Developer:",isSoftwareEngineer, "Type:",typeof isSoftwareEngineer);
+isCodeCorrect = true;
+console.log("Is Code Correct:", isCodeCorrect, "Type:",typeof isCodeCorrect);
+console.log("===============================================================");
+
+console.log("************Difference between null and undefined*****");
+console.log("null value means there is no value.It is simply nothing.It is not equivalent to an empty string or 0 and undefined is when a variable is declared but not assigned ant value");
+
