5bf5229fa6d4df4edf7b88e01de8ec3656c8f431
radhikadod <radhikadod@gmail.com>
2023-05-06 08:54:34 +0530

    commit nd push
    commit nd push

---
+++ 01_variable_assign.js
@@ -0,0 +1,27 @@
+
+
+var myFullName;
+console.log(myFullName);
+
+myFullName="Radhika Dod";
+console.log(myFullName);
+
+myFullName="Radhika Naresh Dod";
+console.log(myFullName);
+
+
+var myLove;
+myLove="I love only JS";
+console.log(myLove);
+
+var firstName="Radhika",lastName="Dod",age="26",address="Bajaj Nagar,Nagpur",pincode=440010,gender="Female",isMarried="Single" ;
+console.log(firstName,lastName,age,address,pincode,gender,isMarried) ;
+
+
+
+
+
+
+
+
+
